e704516d2eabd99685989f1e4d350f611047186d
Ítalo Penaforte <italopenaforte@gmail.com>
2020-06-19 08:42:21 -0300

    refactor: move to a http helper a statusCode 200 and body data

--- src/presentation/controllers/singup/singup.ts
+++ src/presentation/controllers/singup/singup.ts
@@ -1,4 +1,4 @@
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, serverError, ok } from '../../helpers/http-helper'
 import { MissingParamError, InvalidParamError } from '../../erros'
 import { Controller, EmailValidator, HttpResponse, HttpRequest, AddAccount } from './singup-protocol'
 
@@ -35,10 +35,7 @@ export class SingUpController implements Controller {
         email,
         password
       })
-      return {
-        statusCode: 200,
-        body: account
-      }
+      return ok(account)
     } catch (error) {
       return serverError()
     }
