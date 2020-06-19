b42a49271dea5fba8f518845602768fb1c142333
Ítalo Penaforte <italopenaforte@gmail.com>
2020-06-19 16:16:44 -0300

    feat: add ExpressRouterAdapter

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
+import { makeSignUpController } from '../factories/singup'
+import { adaptRoute } from '../adapters/express-route-adpter'
 
 export default (router: Router): void => {
-  router.post('/signup', (request, response) => {
-    response.json({ ok: 'ok' })
-  })
+  router.post('/signup', adaptRoute(makeSignUpController()))
 }
